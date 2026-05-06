37c8aabb87c720f03284ddb1db713dfdca8a9e6e
James Cao <cao.james069@gmail.com>
2026-05-06 18:20:30 +0700

    fix base 2

--- main.cpp
+++ main.cpp
@@ -112,20 +112,6 @@ bool canMove(int dx, int dy){
             }
     return true;
 }
-void removeLine(){
-    int j;
-    for (int i = H-2; i >0 ; i-- ){
-        for (j = 0; j < W-1 ; j++)
-            if (board[i][j] == ' ') break;
-        if (j == W-1){
-            for (int ii = i; ii >0 ; ii-- )
-                for (int j = 0; j < W-1 ; j++ ) board[ii][j] = board[ii-1][j];
-            i++;
-            draw();
-            _sleep(200);
-        }
-    }
-}
 
 int main()
 {
@@ -145,7 +131,7 @@ int main()
         if (canMove(0,1)) y++;
         else {
             block2Board();
-            removeLine();
+            // removeLine();
             x = 5; y = 0; b = rand() % 7;
         }
         block2Board();
