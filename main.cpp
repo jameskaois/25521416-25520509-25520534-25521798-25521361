92fa0a501827312a75b865b6e18c685415ae94c5
tradahoguom <25521798@gm.uit.edu.vn>
2026-05-08 08:26:52 +0700

    add removeLine()

--- main.cpp
+++ main.cpp
@@ -146,6 +146,35 @@ void rotateBlock() {
     }
 }
 
+void removeLine() {
+    for (int i = H - 2; i >= 1; i--) {
+
+        bool full = true;
+
+        for (int j = 1; j < W - 1; j++) {
+            if (board[i][j] == ' ') {
+                full = false;
+                break;
+            }
+        }
+
+        if (full) {
+
+            for (int k = i; k > 1; k--) {
+                for (int j = 1; j < W - 1; j++) {
+                    board[k][j] = board[k - 1][j];
+                }
+            }
+
+            for (int j = 1; j < W - 1; j++) {
+                board[1][j] = ' ';
+            }
+
+            i++;
+        }
+    }
+}
+
 int main()
 {
     srand(time(0));
